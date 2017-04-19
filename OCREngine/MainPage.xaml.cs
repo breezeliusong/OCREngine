d1c7813db92f06dba8507e937d474c87e4e2b632
v-liuson <v-liuson@MININT-9U0J66C>
2017-04-19 09:56:13 +0800

    Add project files.

---
+++ OCREngine/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace OCREngine
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+            // Open a stream for the selected file 
+            StorageFile file = await picker.PickSingleFileAsync();
+            // Ensure a file was selected 
+            if (file != null)
+            {
+                using (IRandomAccessStream fileStream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+                {
+                    // Set the image source to the selected bitmap 
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.DecodePixelWidth = 600; //match the target Image.Width, not shown
+                    await bitmapImage.SetSourceAsync(fileStream);
+                    MyImage.Source = bitmapImage;
+                }
+
+                SoftwareBitmap softwareBitmap;
+
+                using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+                {
+                    // Create the decoder from the stream
+                    BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+                    // Get the SoftwareBitmap representation of the file
+                    softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+                    var Engine = OcrEngine.TryCreateFromUserProfileLanguages();
+                    var result=await Engine.RecognizeAsync(softwareBitmap);
+                    Debug.WriteLine(result.Lines);
+                    Debug.WriteLine(result.Text);
+                    foreach(var line in result.Lines)
+                    {
+                        foreach(var text in line.Words)
+                        {
+                            Debug.WriteLine(text.Text);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
